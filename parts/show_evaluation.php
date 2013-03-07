22acdfda33d29ac528a701a7a9ecaecad13e09aa
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-03-07 14:20:41 -0300

    pequenos ajustes no icone de avaliacao na listagem

--- parts/show_evaluation.php
+++ parts/show_evaluation.php
@@ -1,10 +1,8 @@
 <?php if (get_theme_option('evaluation_show_on_list') && (get_theme_option('evaluation_public_results') || is_user_logged_in())) : ?>
-    <div class="show_evaluation">
-
+    <div class="show_evaluation" title="<?php _e('avaliação', 'consulta'); ?>">
+        <span class="count_object_votes_icon"></span>
     	<span class="count_object_votes">
-    		<span class="count_object_votes_icon"></span>
     		<?php echo count_votes($post->ID); ?>
     	</span>
-
     </div>
 <?php endif; ?>
