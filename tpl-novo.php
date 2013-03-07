624770fc1065f3b70f5ed0a087cd833bbf003db3
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-03-07 14:01:59 -0300

    inicio do desenvolvimento do formulario para enviar novos objetos

--- tpl-novo.php
+++ tpl-novo.php
@@ -5,6 +5,29 @@ if (!get_theme_option('allow_suggested') || !is_user_logged_in()) {
     exit();
 }
 
+$errors = array();
+
+if (!empty($_POST) && wp_verify_nonce($_POST['create_new_object'], 'consulta_create_new_object')) {
+    $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
+    $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING);
+    $object_types = filter_var($_POST['tax_input']['object_type'], FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY);
+    var_dump($object_types); die;
+    $args = array(
+        'post_type' => 'object',
+        'post_title' => $title,
+        'post_content' => $description, 
+    );
+    
+    $postId = wp_insert_post($args);
+    
+    if ($postId) {
+        update_post_meta($postId, '_user_created', true);
+        wp_set_post_terms($postId, $object_types, 'object_type');
+    } else {
+        $errors[] = __('Não foi possível criar o objeto.', 'consulta');
+    } 
+}
+
 get_header();
 
 ?>
@@ -12,6 +35,7 @@ get_header();
 <h2>Adicionar novo objeto</h2>
 
 <form id="new_object" method="post">
+    <?php wp_nonce_field('consulta_create_new_object', 'create_new_object'); ?>
     <div class="clearfix">
         <label for="title"><?php _e('Título', 'consulta'); ?></label>
         <input type="text" id="title" value="" name="title">
@@ -26,14 +50,16 @@ get_header();
         require('wp-admin/includes/template.php'); ?>
         <div class="clearfix">
             <label><?php echo ObjectPostType::get_taxonomy_label('name'); ?></label>
-            <?php wp_terms_checklist(null, array('taxonomy' => 'object_type')); ?>
+            <ul id="object_type">
+                <?php wp_terms_checklist(null, array('taxonomy' => 'object_type')); ?>
+            </ul>
         </div>
         <?php
     }
     ?>
     
     <p>
-        <input type="submit" value="<?php _e('Enviar', 'consulta'); ?>">
+        <input id="new_object_submit" type="submit" value="<?php _e('Enviar', 'consulta'); ?>">
     </p>
 </form>
 
