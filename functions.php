0609ac022b9c3b70fad1d317e56416026bf09185
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-09-19 14:54:45 -0300

    avisa admin que o modulo de consultas depende de uma estrutura de permalinks habilitada

--- functions.php
+++ functions.php
@@ -781,3 +781,10 @@ function consulta_show_user_link() {
     </div>
     <?php
 }
+
+// exibe aviso para o admin caso os permalinks estejam desabilitados
+add_action('admin_notices', function() {
+    if (!get_option('permalink_structure')) {
+        echo '<div id="message" class="error"><p><strong>O módulo de Consultas Públicas depende da estrutura de permalinks habilitada para funcionar corretamente. Por favor altere esta configuração na página <a href="' . admin_url('options-permalink.php') . '">Links permanentes</a>. Você pode escolher qualquer uma das opções, exceto a primeira chamada "Padrão".</strong><p></div>';
+    }
+});
